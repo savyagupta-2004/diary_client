9d29ef3d39bbf54c0ad07d4d6c20fb4cc91e9906
savyagupta-2004 <guptasavya667@gmail.com>
2024-07-11 02:16:54 +0530

    final deploy

--- src/context/diary/DiaryState.js
+++ src/context/diary/DiaryState.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import diaryContext from "./diaryContext";
 
 const DiaryState = (props) => {
-  const host = "http://localhost:5000";
+  const host = "https://deardiary-b.vercel.app";
   const diariesInitial = [];
   const [diaries, setdiary] = useState(diariesInitial);
 
